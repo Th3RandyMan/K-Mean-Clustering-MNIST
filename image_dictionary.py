aad13489882b3daacdbccd8363786fcae8395895
Randall Fowler <rlfowler@ucdavis.edu>
2024-04-17 14:49:12 -0700

    working py file

--- image_dictionary.py
+++ image_dictionary.py
@@ -1,7 +1,9 @@
 import os
 import numpy as np
-from sklearn.cluster import KMeans
+from sklearn.cluster import KMeans, MiniBatchKMeans
 from sklearn.metrics import euclidean_distances
+from sklearn.feature_extraction.image import extract_patches_2d, reconstruct_from_patches_2d
+
 
 class ImageDictionary:
 
@@ -48,6 +50,7 @@ class ImageDictionary:
 
         os.environ['OMP_NUM_THREADS'] = '1'  # to avoid warning message
         self.kmeans = KMeans(n_clusters=self.n_clusters, n_init='auto', verbose=1)    # This can take about 10-15 minutes
+        # self.kmeans = MiniBatchKMeans(n_clusters=self.n_clusters, n_init='auto', verbose=1)
         self.kmeans.fit(X)
         self.Phi = self.kmeans.cluster_centers_ # could remove top 3 lines to save space
 
@@ -55,6 +58,10 @@ class ImageDictionary:
         if X is None:
             X = self.images
 
+        # data = []
+        # for img in X:
+        #     data.extend(extract_patches_2d(img, (self.patch_size, self.patch_size)))
+        # return np.array(data) # returns (n_patches, patch_size, patch_size)
         height, width = X[0].shape
         num_windows = (int((width-self.patch_size)/self.stride)+1, int((height-self.patch_size)/self.stride)+1)
         patches = np.zeros((len(X), np.prod(num_windows), self.patch_size, self.patch_size), dtype=type(X.flat[0]))
@@ -63,10 +70,7 @@ class ImageDictionary:
             for i in range(0, height - self.patch_size + 1, self.stride):
                 for j in range(0, width - self.patch_size + 1, self.stride):
                     patches[k, i*num_windows[0]+j] = image[i:i+self.patch_size, j:j+self.patch_size]
-        return patches
-    
-    def _collect_patches(self, X:np.ndarray):
-        pass
+        return patches  # returns (n_images, n_patches, patch_size, patch_size)
         
     def _flatten(self, X):
         return X.reshape(*X.shape[0:2], -1)
@@ -114,51 +118,57 @@ class ImageDictionary:
         plt.tight_layout()
         try:
             # if in jupyter notebook
-            if get_ipython().__class__.__name__ == 'ZMQInteractiveShell':
-                plt.title(figname)
+            if get_ipython().__class__.__name__ == 'ZMQInteractiveShell': # type: ignore
+                plt.title(figname[:-4])
                 plt.show()
             else:
                 if not os.path.exists("figures"):
                     os.makedirs("figures")
-                if "figures" not in filename:
-                    filename = "figures/"+filename
+                if "figures" not in figname:
+                    figname = "figures/"+figname
                 plt.savefig(figname)
                 plt.close()
         except NameError:
             if not os.path.exists("figures"):
                 os.makedirs("figures")
-            if "figures" not in filename:
-                filename = "figures/"+filename
+            if "figures" not in figname:
+                figname = "figures/"+figname
             plt.savefig(figname)
             plt.close()
 
-    def reconstruct(self, X:np.ndarray, save:bool = False, filename:str = "reconstructed.npy"):
+    def reconstruct(self, X:np.ndarray, save:bool = False, filename:str = "reconstructed.npy") -> np.ndarray:
         if len(X.shape) == 2:
             X = X.reshape(1, *X.shape)  # add batch dimension
+        
         patches = self._transform(X)
+        Y = np.zeros(X.shape)
+        for i, img_patches in enumerate(patches):
+            Y[i] = reconstruct_from_patches_2d(img_patches, X.shape[1:])
 
         if save:
-            np.save(filename, patches)
+            np.save(filename, Y)
+        return Y
 
 
     def _transform(self, X:np.ndarray):
         patches = self._patches(X)  # Get patches
         patches = self._flatten(patches)    # Flatten for normalization
-        shape = patches.shape
+        shape = patches.shape   # (n_images, n_patches, patch_size * patch_size)
         patches = patches.reshape(-1, patches.shape[-1])
-        patches -= np.mean(patches, axis=0)
-        patches /= np.std(patches, axis=0)
+        mean = np.mean(patches, axis=0)
+        var = np.std(patches, axis=0)
+        patches -= mean
+        patches /= var # added, might remove
+
 
         dist = euclidean_distances(patches, self.Phi)
-        new_patches = self.Phi[np.argmin(dist, axis=1)].reshape(shape)
-        return self._unflatten(new_patches)
+        new_patches = self.Phi[np.argmin(dist, axis=1)]
+        new_patches += mean
+        new_patches *= var  # added, might remove
+        new_patches -= new_patches.min()
+        new_patches /= new_patches.max()
+        return self._unflatten(new_patches.reshape(shape))
 
-        #patches = self._unflatten(patches)
-        
-        # for image in patches:
-        #     pass    # See if shaping is done correctly...
-        # return euclidean_distances(patches, self.Phi)
-        
 
 if __name__ == "__main__":
     from keras.datasets import mnist
@@ -166,7 +176,7 @@ if __name__ == "__main__":
     x_train = x_train.astype('float32') / 255.0
     x_test = x_test.astype('float32') / 255.0
     
-    if not os.path.exists("dictionary.npy"):
+    if not os.path.exists("codebooks/dictionary.npy"):
         img_dict = ImageDictionary(x_train)
         img_dict.save()
     else:
@@ -176,8 +186,38 @@ if __name__ == "__main__":
     img_dict.plot_dictionary()
     print(img_dict)
 
-    img_dict.reconstruct(x_test, save=True)
+    recon_imgs = img_dict.reconstruct(x_test, save=True)
 
-    pass
 
+
+    import random
+    import matplotlib.pyplot as plt
+
+    # Select 5 random indices
+    random_indices = random.sample(range(len(recon_imgs)), 5)
+
+    # Plot the images
+    fig, axes = plt.subplots(2, 5, figsize=(15, 3))
+
+    for i, ax in enumerate(axes.flatten()):
+        # Display the image
+        if i < 5:
+            ax.imshow(recon_imgs[random_indices[i]], cmap='gray')
+            ax.set_title("Reconstructed Image")
+        else:
+            ax.imshow(x_test[random_indices[i-5]], cmap='gray')
+            ax.set_title("Original Image")
+
+        # # Set the title with the label
+        # ax.set_title(f"Label: {y_test[random_indices[i]]}")
+
+        # Remove the axis labels
+        ax.axis('off')
+
+    if not os.path.exists("figures"):
+        os.makedirs("figures")
+    plt.savefig("figures/Reconstructed_Images.png")
+
+    # Show the plot
+    plt.show()
         
